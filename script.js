78a386ec7bdd27c5990abb14d1c621a53a37e4a9
Ella22011 <142878147+Ella22011@users.noreply.github.com>
2023-10-05 14:12:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,20 +5,16 @@ document.addEventListener('DOMContentLoaded', function () {
     let isKissing = false;
 
     kissButton.addEventListener('click', () => {
-        // Move the images towards each other
         floatingImages[0].style.transform = 'translateX(38px)';
         floatingImages[1].style.transform = 'translateX(-10px)';
         
-        // Show the "muisk" text
         muiskText.style.display = 'block';
 
-        // After a short delay, move the images back to their original positions
         setTimeout(() => {
             floatingImages[0].style.transform = 'translateX(0)';
             floatingImages[1].style.transform = 'translateX(0)';
             
-            // Hide the "muisk" text again
             muiskText.style.display = 'none';
-        }, 800); // Adjust the delay (in milliseconds) as needed
+        }, 800);
     });
 });
